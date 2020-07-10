07fc1d5c683d227b0564c8e232f6017f13788d43
Zeeson <olabrazanislam@gmail.com>
2020-07-10 14:15:47 -0700

    All files updatted

---
+++ js/makeup.js
@@ -0,0 +1,9 @@
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev')
+   
+  });
